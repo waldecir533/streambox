b2a390f75bf7f0ecf61f68f6f7c425246918dc72
waldecir533 <3.15875198e+08+waldecir533@users.noreply.github.com>
2026-08-12 19:38:08 +0000

    ci: remove import inutilizado que fazia a análise falhar no GitHub Actions

--- test/preferences_service_test.dart
+++ test/preferences_service_test.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:streambox/models/channel.dart';
 import 'package:streambox/services/preferences_service.dart';
 
